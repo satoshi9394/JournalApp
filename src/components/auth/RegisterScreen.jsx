bd4954efe1c240ff87ec533daa2e22e02d7659e3
Angel Santillan <angelsb94@gmail.com>
2021-01-21 22:08:10 -0600

    agregar estilos a las vistas de registro

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const RegisterScreen = () => {
   return (
-    <div>
-      <h1>RegisterScreen</h1>
-    </div>
+    <>
+      <h3 className="auth__title">Registro</h3>
+      <form>
+        <input
+          type="name"
+          placeholder="Nombre"
+          name="name"
+          className="auth__input"
+          autoComplete="off"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          className="auth__input"
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          className="auth__input"
+        />
+        <input
+          type="password"
+          placeholder="Confirma Password"
+          name="password2"
+          className="auth__input"
+        />
+        <button
+          className="btn btn-primary btn-block mb-5"
+          type="submit"
+          disabled={ false }
+        >
+          Registrate
+        </button>
+        <Link to="/auth/login" className="link">
+          Ya estas registrado?
+        </Link>
+      </form>
+    </>
   )
 }
 
