80cd930bd0c911ad942ffd38ea2279af5534a63a
Angel Santillan <angelsb94@gmail.com>
2021-01-29 21:17:35 -0600

    configuracion de redux principio

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
+import { store } from './store/stores'
+
 
 const JournalApp = () => {
   return (
-    <>
+    <Provider store={ store }>
       <AppRouter/>
-    </>
+    </Provider>
   )
 }
 
